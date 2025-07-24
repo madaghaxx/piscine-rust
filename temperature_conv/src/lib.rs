db83be1890ec8f5495dd4173af7c89e523ddf47e
madahgaxx <aberhili0man@gmail.com>
2025-07-24 11:04:52 +0100

    math

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,8 +1,9 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    let empty: f64 = (1/(9 / 5) ) as f64;
-    (f - 32.0) * (5.0 / 9.0) + empty;
+    let empty: f64 = (1 / (9 / 5)) as f64;
+    (f - 32.0) * empty
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
-    c * (9.0 / 5.0) + 32.0
+    let empty: f64 = (1 / (9 / 5)) as f64;
+    c * (1 / empty) + 32.0
 }
