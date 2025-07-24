3634df9e1a8799036b8631df19f57b11d9dfe985
Aymane Berhili <aberhili0man@gmail.com>
2025-07-24 13:07:22 +0100

    lfarsi gal

--- fibonacci2/src/lib.rs
+++ fibonacci2/src/lib.rs
@@ -1,3 +1,3 @@
 pub fn fibonacci(n: u32) -> u32 {
-    if n == 0 { 0 } else if n == 1 { 1 } else { fibonacci(n - 1) + fibonacci(n + 1) }
+    if n <= 1 { n } else { fibonacci(n - 1) + fibonacci(n + 1) }
 }
