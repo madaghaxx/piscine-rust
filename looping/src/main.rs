44dd828e4c689080ee38ef2c56bd8c96bf731d14
Aymane Berhili <aberhili0man@gmail.com>
2025-07-24 12:19:56 +0100

    moving the mutable inside

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -1,15 +1,16 @@
 use std::io;
+
 fn main() {
-    let correct_answer = "The letter e";
-    let mut answer = String::new();
     let mut trials = 0;
     loop {
+        let mut answer = String::new();
         println!(
             "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?"
         );
         io::stdin().read_line(&mut answer).expect("walo");
         trials += 1;
-        if answer.trim() == correct_answer {
+        println!("{}",answer.trim() == "The letter e");
+        if answer.trim() == "The letter e" {
             println!("Number of triels: {}", trials);
             break;
         }
