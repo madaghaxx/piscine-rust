85342b2e20c6282e6e5e039b2f84e4084f8ff60a
Aymane Berhili <aberhili0man@gmail.com>
2025-07-24 14:49:40 +0100

    tuples kay3sbo

---
+++ tuples_refs/src/lib.rs
@@ -0,0 +1,11 @@
+pub struct Student(pub u32, pub String, pub String);
+pub fn id(student: &Student) -> u32 {
+    student.0
+}
+pub fn first_name(student: &Student) -> &str {
+    student.1.as_str()
+}
+
+pub fn last_name(student: &Student) -> &str {
+    student.2.as_str()
+}
