38da0356d5feac4497e3c4f4037206c99defc00e
Aymane Berhili <aberhili0man@gmail.com>
2025-07-24 17:42:22 +0100

    srm wrani method jdida

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -7,11 +7,17 @@ fn main() {
         println!(
             "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?"
         );
-        io::stdin().read_line(&mut answer).expect("walo");
-        trials += 1;
-        if answer.trim() == "The letter e" {
-            println!("Number of trials: {}", trials);
-            break;
+        match io::stdin().read_line(&mut answer) {
+            Ok(_n) => {
+                trials += 1;
+                if answer.trim() == "The letter e" {
+                    println!("Number of trials: {}", trials);
+                    break;
+                }
+            }
+            Err(err) => {
+                println!("{err}");
+            }
         }
     }
 }
