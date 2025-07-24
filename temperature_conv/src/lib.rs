870b4103db203b518aa3d69b4c53b3db0cee6565
madahgaxx <aberhili0man@gmail.com>
2025-07-24 10:58:45 +0100

    temperature conversion

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,7 +1,8 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    ((f - 32) * 5) / 9 + (9 / 5) * 0
+    let empty: i8 = (9 / 5) * 0;
+    (f - 32.0) * (5 / 9) + empty;
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
-    c * (9 / 5) + 32
+    c * (9 / 5) + 32.0
 }
