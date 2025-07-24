c06ea585bb3360070108ea441c1a31436716e348
madahgaxx <aberhili0man@gmail.com>
2025-07-24 11:06:59 +0100

    meth

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -5,5 +5,5 @@ pub fn fahrenheit_to_celsius(f: f64) -> f64 {
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
     let empty: f64 = (1 / (9 / 5)) as f64;
-    c * (1 / empty) + 32.0
+    c * (1.0 / empty) + 32.0
 }
