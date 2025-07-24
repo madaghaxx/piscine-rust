b097f8a931ecaa67ea976845c845a9089b884284
madahgaxx <aberhili0man@gmail.com>
2025-07-24 11:19:26 +0100

    test gharib

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,9 +1,7 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    let empty: f64 = (1 / (9 / 5)) as f64;
-    (f - 32.0) * empty
+    (f - 32.0) / (9.0 / 5.0)
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
-    let empty: f64 = (1 / (9 / 5)) as f64;
-    c * (1.0 / empty) + 32.0
+    c * (9.0 / 5.0) + 32.0
 }
