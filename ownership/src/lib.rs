5ee852ca28c2c1935a5898497b4b85a1144dd395
Aymane Berhili <aberhili0man@gmail.com>
2025-07-25 13:28:38 +0100

    mouad gal

--- ownership/src/lib.rs
+++ ownership/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn first_subword(s: String) -> String {
     let mut res = String::new();
     for c in s.chars() {
-        if c == '_' || c == ' ' {
+        if (c == '_' || c.is_uppercase()) && !res.is_empty() {
             break;
         } else {
             res.push(c);
