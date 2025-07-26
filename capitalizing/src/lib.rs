96ddecd310abbc10252c23f70a60ec85795ff824
Aymane Berhili <aberhili0man@gmail.com>
2025-07-26 16:31:47 +0100

    well well well

---
+++ capitalizing/src/lib.rs
@@ -0,0 +1,30 @@
+pub fn capitalize_first(input: &str) -> String {
+    let mut chars = input.chars();
+    match chars.next() {
+        Some(first) => first.to_uppercase().collect::<String>() + chars.as_str(),
+        None => String::from("walo"),
+    }
+}
+
+pub fn title_case(input: &str) -> String {
+    input
+        .split_whitespace()
+        .map(|word| capitalize_first(word))
+        .collect::<Vec<_>>()
+        .join(" ")
+}
+
+pub fn change_case(input: &str) -> String {
+    input
+        .chars()
+        .map(|c| {
+            if c.is_lowercase() {
+                c.to_uppercase().collect::<String>()
+            } else if c.is_uppercase() {
+                c.to_lowercase().collect::<String>()
+            } else {
+                c.to_string()
+            }
+        })
+        .collect()
+}
