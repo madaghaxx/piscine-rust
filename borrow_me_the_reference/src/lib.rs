e4e8a33ecb99307d01e0eb26d44f70c6a20a9ba0
Aymane Berhili <aberhili0man@gmail.com>
2025-07-26 11:53:19 +0100

    dik zawja ai w farsi helped

--- borrow_me_the_reference/src/lib.rs
+++ borrow_me_the_reference/src/lib.rs
@@ -1,22 +1,48 @@
 pub fn delete_and_backspace(s: &mut String) {
     let mut result = String::new();
-    let chars: Vec<char> = s.chars().collect();
     let mut i = 0;
-    while i < chars.len() {
-        match chars[i] {
-            '-' => {
-                result.pop();
+    for character in s.chars() {
+        if character == '-' {
+            result.pop();
+        } else if character == '+' {
+            i += 1;
+        } else {
+            if i != 0 {
+                i -= 1;
+                continue;
             }
-            '+' => {
-                // mrdatni
-            }
-            c => {
-                result.push(c);
-            }
+            result.push(character);
         }
-        i += 1;
     }
+
     *s = result;
 }
+pub fn do_operations(v: &mut [String]) {
+    for s in v.iter_mut() {
+        let mut chars = s.chars();
+        let mut lwl = String::new();
+        let mut zawj = String::new();
+        let mut op = None;
+        while let Some(c) = chars.next() {
+            if c == '+' || c == '-' {
+                op = Some(c);
+                break;
+            }
+            lwl.push(c);
+        }
+        for c in chars {
+            zawj.push(c);
+        }
 
-// pub fn do_operations(v: &mut [String]) {}
+        if let (Ok(a), Some(op), Ok(b)) = (lwl.parse::<i32>(), op, zawj.parse::<i32>()) {
+            let res = match op {
+                '+' => a + b,
+                '-' => a - b,
+                _ => {
+                    continue;
+                }
+            };
+            *s = res.to_string();
+        }
+    }
+}
--- push.sh
+++ push.sh
@@ -1,6 +1,11 @@
 #bin/bash
 # before running the script add your remote by running:
 # git remote add github "your@github.email"
+
+#git config --global credential.helper store
+#piscine-rust git:(main) ✗ git config --global user.email aberhili0man@gmail.com
+#piscine-rust git:(main) ✗ git remote add github https://github.com/madaghaxx/piscine-rust
+
 git add .
 git commit -m "$1"
 git push 
