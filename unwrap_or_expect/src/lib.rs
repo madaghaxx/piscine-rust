8422d9ff739a342436342a5d61c477a7a9870248
aberhili0man@gmail.com <aberhili0man@gmail.com>
2025-07-31 14:45:19 +0100

    error handling

---
+++ unwrap_or_expect/src/lib.rs
@@ -0,0 +1,21 @@
+pub enum Security {
+    Unknown,
+    Message,
+    Warning,
+    NotFound,
+    UnexpectedUrl,
+}
+
+pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String {
+    match security_level {
+        Security::Unknown => server.unwrap().to_string(),
+        Security::Message => server.expect("ERROR: program stops").to_string(),
+        Security::Warning => server.unwrap_or("WARNING: check the server").to_string(),
+        Security::NotFound =>
+            match server {
+                Ok(msg) => msg.to_string(),
+                Err(msg) =>(format!("Not found: {}", msg)).to_string(),
+            }
+        Security::UnexpectedUrl => server.unwrap_err().to_string(),
+    }
+}
