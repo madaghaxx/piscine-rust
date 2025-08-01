e11ef20fdf01b56dade095cf6d64bd491f31fa99
Christopher  Fremond <dev@01-edu.org>
2025-08-01 09:15:07 +0000

    Update profanity_filter/src/lib.rs

--- profanity_filter/src/lib.rs
+++ profanity_filter/src/lib.rs
@@ -1,8 +1,17 @@
 pub fn check_ms(message: &str) -> Result<&str, &str> {
     let sensored = String::from("strupid");
-    if message.to_lowercase().contains(&sensored) && message!=""{
+    if message.to_lowercase().contains(&sensored) && message != "" {
         Err("ERROR: illegal")
     } else {
         Ok(message)
+    }
+}
+pub struct Message {
+    message: String,
+}
+impl Message {
+    pub fn new(mut m1: String, m2: String) -> String {
+        m1 += &(" ".to_string()+&m2);
+      m1
     }
 }
