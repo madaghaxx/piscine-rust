25e74dd922fc3b478952623590916c9ed574aaf5
Aymane Berhili <aberhili0man@gmail.com>
2025-08-01 12:09:17 +0100

    cipher but with a lot of help

---
+++ cipher/src/lib.rs
@@ -0,0 +1,27 @@
+#[derive(Debug, PartialEq)]
+pub struct CipherError {
+    expected: String,
+}
+
+pub fn cipher(original: &str, ciphered: &str) -> Result<(), CipherError> {
+    let solution: String = original
+        .chars()
+        .map(|c| {
+            if c.is_ascii_alphabetic() {
+                let base = if c.is_ascii_lowercase() { b'a' } else { b'A' };
+                let offset = (c as u8) - base;
+                (base + (25 - offset)) as char
+            } else {
+                c
+            }
+        })
+        .collect();
+
+    if &ciphered == &solution {
+        Ok(())
+    } else {
+        Err(CipherError {
+            expected: solution,
+        })
+    }
+}
