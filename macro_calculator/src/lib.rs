98200e141d59a7fe1e7b3e4adf1229101ba66594
aberhili0man@gmail.com <aberhili0man@gmail.com>
2025-08-02 10:38:02 +0100

    What is loooove

--- macro_calculator/src/lib.rs
+++ macro_calculator/src/lib.rs
@@ -10,20 +10,33 @@ pub struct Food {
 
 pub fn calculate_macros(foods: &[Food]) -> json::JsonValue {
     let mut cals = 0.0;
-    let mut cabs = 0.0;
+    let mut carbs = 0.0;
     let mut fats = 0.0;
     let mut proteins = 0.0;
+
     for food in foods {
         let calories: f64 = food.calories.1[0..food.calories.1.len() - 4].parse().unwrap();
         cals += calories * food.nbr_of_portions;
-        cabs += food.carbs * food.nbr_of_portions;
+        carbs += food.carbs * food.nbr_of_portions;
         fats += food.fats * food.nbr_of_portions;
         proteins += food.proteins * food.nbr_of_portions;
     }
+
+    let format_number = |n: f64| -> f64 {
+        let rounded = (n * 100.0).round() / 100.0;
+        if rounded.fract() == 0.0 {
+            rounded
+        } else if (rounded * 10.0).fract() == 0.0 {
+            (rounded * 10.0).round() / 10.0
+        } else {
+            rounded
+        }
+    };
+
     object! {
-        cals:cals,
-        cabs:cabs,
-        proteins:proteins,
-        fats:fats
+        cals: format_number(cals),
+        carbs: format_number(carbs),
+        proteins: format_number(proteins),
+        fats: format_number(fats)
     }
 }
