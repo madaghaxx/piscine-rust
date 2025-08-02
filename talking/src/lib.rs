cf6b6610ea2ac17d2208be62051a42e6f9a450c9
aberhili0man@gmail.com <aberhili0man@gmail.com>
2025-08-02 12:38:56 +0100

    yes yes totally my fault

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -1,9 +1,9 @@
 pub fn talking(text: &str) -> &str {
-    if text.is_empty() {
+    if text.trim().is_empty() {
         "Just say something!"
-    } else if text.ends_with('?') && text == text.to_uppercase() {
+    } else if text.ends_with('?') && text == text.to_uppercase() && text.chars().any(|c| c.is_alphabetic()) {
         "Quiet, I am thinking!"
-    } else if text == text.to_uppercase() {
+    } else if text == text.to_uppercase() && text.chars().any(|c| c.is_alphabetic()) {
         "There is no need to yell, calm down!"
     } else if text.ends_with('?') {
         "Sure."
