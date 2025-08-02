c8b3e086ea7bbe1d946c823c5f1364e6a4965af5
aberhili0man@gmail.com <aberhili0man@gmail.com>
2025-08-02 14:29:13 +0100

    I will be using this for ever

---
+++ rot/src/lib.rs
@@ -0,0 +1,16 @@
+pub fn rotate(input: &str, key: i8) -> String {
+    let mut coded = "".to_string();
+    if key == 0 {
+        return (&input).to_string();
+    }
+    for c in input.chars() {
+        if c.is_alphabetic() {
+            let base = if c.is_lowercase() { b'a' } else { b'A' };
+            let shifted = (((((c as u8) - base) as i8) + key) % 26) as u8;
+            coded += &char::from(base + shifted).to_string();
+        } else {
+            coded += &c.to_string();
+        }
+    }
+    coded
+}
--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -1,7 +1,11 @@
 pub fn talking(text: &str) -> &str {
     if text.trim().is_empty() {
         "Just say something!"
-    } else if text.ends_with('?') && text == text.to_uppercase() && text.chars().any(|c| c.is_alphabetic()) {
+    } else if
+        text.ends_with('?') &&
+        text == text.to_uppercase() &&
+        text.chars().any(|c| c.is_alphabetic())
+    {
         "Quiet, I am thinking!"
     } else if text == text.to_uppercase() && text.chars().any(|c| c.is_alphabetic()) {
         "There is no need to yell, calm down!"
