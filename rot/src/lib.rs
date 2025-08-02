ff03f46bc050186ac95972e687fe97591d50450a
aberhili0man@gmail.com <aberhili0man@gmail.com>
2025-08-02 14:30:25 +0100

    l9isma i i9lidia f rust:rem_euclid

--- rot/src/lib.rs
+++ rot/src/lib.rs
@@ -6,7 +6,7 @@ pub fn rotate(input: &str, key: i8) -> String {
     for c in input.chars() {
         if c.is_alphabetic() {
             let base = if c.is_lowercase() { b'a' } else { b'A' };
-            let shifted = (((((c as u8) - base) as i8) + key) % 26) as u8;
+            let shifted = ((((c as u8) - base) as i8) + key).rem_euclid(26) as u8;
             coded += &char::from(base + shifted).to_string();
         } else {
             coded += &c.to_string();
