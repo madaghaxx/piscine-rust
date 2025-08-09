1fd403a8249b8a13af4d1dac9defca1fa4c4a0c6
Aymane Berhili <aberhili0man@gmail.com>
2025-08-09 12:44:25 +0100

    aaaaaaaaaaaaaaaaaaa

---
+++ generics_list/src/lib.rs
@@ -0,0 +1,39 @@
+#[derive(Clone, Debug)]
+pub struct List<T> {
+    pub head: Option<Node<T>>,
+}
+
+#[derive(Clone, Debug)]
+pub struct Node<T> {
+    pub value: T,
+    pub next: Option<Box<Node<T>>>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> List<T> {
+        List {
+            head: None,
+        }
+    }
+    pub fn push(&mut self, value: T) {
+        let node = Node {
+            value: value,
+            next: self.head.take().map(Box::new),
+        };
+        self.head = Some(node);
+    }
+    pub fn pop(&mut self) {
+        if let Some(node) = self.head.take() {
+            self.head = node.next.map(|b| *b);
+        }
+    }
+    pub fn len(&self) -> usize {
+        let mut count = 0;
+        let mut current = self.head.as_ref();
+        while let Some(node) = current {
+            count += 1;
+            current = node.next.as_deref();
+        }
+        count
+    }
+}
