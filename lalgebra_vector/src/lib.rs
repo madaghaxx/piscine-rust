0485e51710aa5287a7180ea45cce9fd4dd1b8022
Aymane Berhili <aberhili0man@gmail.com>
2025-08-14 10:55:00 +0100

    I ahte algebra

---
+++ lalgebra_vector/src/lib.rs
@@ -0,0 +1,40 @@
+use std::ops::*;
+pub trait Scalar: Copy + Add<Output = Self> + Mul<Output = Self> {}
+
+impl<T: Copy + Add<Output = Self> + Mul<Output = Self>> Scalar for T {}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Vector<T: Scalar>(pub Vec<T>);
+
+impl<T: Scalar > Add for Vector<T> {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        let mut result = Vec::new();
+        for (a, b) in self.0.iter().zip(other.0.iter()) {
+            result.push(*a + *b);
+        }
+        Self(result)
+    }
+}
+
+impl<T: Scalar> Vector<T> {
+    pub fn new() -> Self {
+        Self(Vec::new())
+    }
+
+    pub fn dot(&self, other: &Self) -> Option<T> {
+        if self.0.len() != other.0.len() {
+            return None;
+        }
+        let mut sum = None;
+        for (a, b) in self.0.iter().zip(other.0.iter()) {
+            let product = *a * *b;
+            sum = match sum {
+                None => Some(product),
+                Some(s) => Some(s + product),
+            };
+        }
+        sum
+    }
+}
