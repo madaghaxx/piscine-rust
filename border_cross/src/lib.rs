8e7fc0fc6daca6528dce6f151df78109121b54d0
Aymane Berhili <aberhili0man@gmail.com>
2025-08-14 12:25:49 +0100

    Okey the given main is a trap! notes taken

---
+++ border_cross/src/lib.rs
@@ -0,0 +1,47 @@
+#[allow(dead_code)]
+pub struct Car<'a> {
+    pub plate_nbr: &'a str,
+    pub model: &'a str,
+    pub horse_power: u32,
+    pub year: u32,
+}
+
+#[allow(dead_code)]
+pub struct Truck<'a> {
+    pub plate_nbr: &'a str,
+    pub model: &'a str,
+    pub horse_power: u32,
+    pub year: u32,
+    pub load_tons: u32,
+}
+
+pub trait Vehicle {
+    fn model(&self) -> &str;
+    fn year(&self) -> u32;
+}
+
+impl Vehicle for Truck<'_> {
+    fn model(&self) -> &str {
+        self.model
+    }
+    fn year(&self) -> u32 {
+        self.year
+    }
+}
+
+impl Vehicle for Car<'_> {
+    fn model(&self) -> &str {
+        self.model
+    }
+    fn year(&self) -> u32 {
+        self.year
+    }
+}
+
+pub fn all_models<'a>(list: Vec<&'a dyn Vehicle>) -> Vec<&'a str> {
+    let mut res = Vec::new();
+    for car in list {
+        res.push(car.model());
+    }
+    res
+}
