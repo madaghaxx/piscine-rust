80cecf5a74a5f8cd8c424e93ef413cabd854fce7
Aymane Berhili <aberhili0man@gmail.com>
2025-08-14 10:33:46 +0100

    new things

---
+++ arrange_it/src/lib.rs
@@ -0,0 +1,12 @@
+pub fn arrange_phrase(phrase: &str) -> String {
+    let mut words: Vec<&str> = phrase.split_whitespace().collect();
+    
+    words.sort_by_key(|word| {
+        word.chars().find(|c| c.is_ascii_digit()).unwrap_or('0')
+    });
+    
+    words.iter()
+        .map(|word| word.chars().filter(|c| !c.is_ascii_digit()).collect::<String>())
+        .collect::<Vec<String>>()
+        .join(" ")
+}
