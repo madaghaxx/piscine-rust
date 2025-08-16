b1812a1c453d31ad004b4d95a25a1a1be33f766d
Aymane Berhili <aberhili0man@gmail.com>
2025-08-16 15:47:14 +0100

    It sounded hard

---
+++ highest/src/lib.rs
@@ -0,0 +1,30 @@
+#[derive(Debug)]
+pub struct Numbers<'a> {
+    numbers: &'a [u32],
+}
+
+impl<'a> Numbers<'a> {
+    pub fn new(numbers: &'a [u32]) -> Self {
+        Self {
+            numbers,
+        }
+    }
+
+    pub fn list(&self) -> &[u32] {
+        self.numbers
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.numbers.last().copied()
+    }
+
+    pub fn highest(&self) -> Option<u32> {
+        self.numbers.iter().max().copied()
+    }
+
+    pub fn highest_three(&self) -> Vec<u32> {
+        let mut sorted = self.numbers.to_vec();
+        sorted.sort_by(|a, b| b.cmp(a));
+        sorted.into_iter().take(3).collect()
+    }
+}
