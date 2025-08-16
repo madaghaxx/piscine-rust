46a6d6dd9d3a22dc2f33c231688005fafbc772c5
Aymane Berhili <aberhili0man@gmail.com>
2025-08-16 14:19:51 +0100

    sales

--- sales/src/lib.rs
+++ sales/src/lib.rs
@@ -21,19 +21,33 @@ impl Cart {
             receipt: vec![],
         }
     }
+    
     pub fn insert_item(&mut self, s: &Store, ele: String) {
-        for (i, j) in &s.products {
-            if i.to_string() == ele {
-                self.items.push((i.to_string(), *j));
-                self.receipt.push(*j);
+        for (name, price) in &s.products {
+            if *name == ele {
+                self.items.push((name.clone(), *price));
+                break;
             }
         }
     }
+    
     pub fn generate_receipt(&mut self) -> Vec<f32> {
-        let mut res = vec![];
-        for i in &self.receipt {
-            res.push(*i);
+        let mut prices: Vec<f32> = self.items.iter().map(|(_, price)| *price).collect();
+        prices.sort_by(|a, b| a.partial_cmp(b).unwrap());
+        
+        let total_items = prices.len();
+        let free_items = total_items / 3;
+        
+        if free_items > 0 {
+            let total_price: f32 = prices.iter().sum();
+            let free_price: f32 = prices.iter().take(free_items).sum();
+            let adjusted_total = total_price - free_price;
+            
+            let ratio = adjusted_total / total_price;
+            prices = prices.into_iter().map(|price| (price * ratio * 100.0).round() / 100.0).collect();
         }
-        res
+        
+        self.receipt = prices.clone();
+        prices
     }
 }
