9b2e87502d785c38c21bbc81fdeef81a52cfe5bb
Aymane Berhili <aberhili0man@gmail.com>
2025-09-04 09:52:05 +0100

    scalar

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+cat <<'EOF' >> ~/.zshrc
+alias run="clear && cargo run"
+push() {
+	cd ~/Desktop/piscine-rust/
+	git add .
+	git commit -m "$1"
+	git push 
+	git push github
+}
+
+lib() {
+	cargo new --lib "$1"
+	cd "$1"
+	touch src/main.rs
+	: > src/lib.rs
+	code src/main.rs
+	code src/lib.rs
+}
+EOF
+clear
