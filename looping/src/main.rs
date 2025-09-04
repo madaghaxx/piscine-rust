60c083c879d107aaa4c3b304079d833132a20855
Aymane Berhili <aberhili0man@gmail.com>
2025-09-04 10:36:25 +0100

    I might be smart but not enough

---
+++ looping/src/main.rs
@@ -0,0 +1,17 @@
+use std::io;
+fn main() {
+    loop {
+        let mut trials = 1;
+        let answer = "The letter e\n".to_string();
+        println!(
+            "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?"
+        );
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        trials += 1;
+        if input == answer {
+            println!("Number of trials: {trials}");
+            break;
+        }
+    }
+}
