2a965b4395c46b87e451723247fd33cccb30a2ee
Aymane Berhili <aberhili0man@gmail.com>
2025-09-04 10:45:35 +0100

    deffinetly I am stupid

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -1,17 +1,17 @@
 use std::io;
 fn main() {
+    let mut trials = 0;
+    let answer = "The letter e\n".to_string();
     loop {
-        let mut trials = 1;
-        let answer = "The letter e\n".to_string();
         println!(
             "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?"
         );
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
+        trials += 1;
         if input == answer {
             println!("Number of trials: {trials}");
             break;
         }
-        trials += 1;
     }
 }
