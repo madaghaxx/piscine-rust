503e6e89cdcb34835418d81f51d4c0f7a2742b10
Aymane Berhili <aberhili0man@gmail.com>
2025-09-04 10:40:38 +0100

    I am stupid or the example is wromg

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -6,12 +6,12 @@ fn main() {
         println!(
             "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?"
         );
-        trials += 1;
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
         if input == answer {
             println!("Number of trials: {trials}");
             break;
         }
+        trials += 1;
     }
 }
