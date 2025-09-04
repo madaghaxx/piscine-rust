73f3830b2128123b84146023f4452adf392f25ad
Aymane Berhili <aberhili0man@gmail.com>
2025-09-04 10:39:47 +0100

    awdee

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -6,9 +6,9 @@ fn main() {
         println!(
             "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?"
         );
+        trials += 1;
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-        trials += 1;
         if input == answer {
             println!("Number of trials: {trials}");
             break;
