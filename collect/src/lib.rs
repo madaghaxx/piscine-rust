ea52ec6511e4900e7a050d5719080fb3b023570f
Aymane Berhili <aberhili0man@gmail.com>
2025-09-06 10:34:41 +0100

    really?

---
+++ collect/src/lib.rs
@@ -0,0 +1,10 @@
+pub fn bubble_sort(arr: &mut [i32]) {
+    let n = arr.len();
+    for i in 0..n {
+        for j in 0..n - 1 - i {
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1);
+            }
+        }
+    }
+}
