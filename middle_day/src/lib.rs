f26c6450dfd70844f054b8709b2f5e2d3645bb52
Aymane Berhili <aberhili0man@gmail.com>
2025-09-12 11:17:38 +0100

    nsit n9ra subject

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -2,7 +2,9 @@ use chrono::{NaiveDate, Datelike, Weekday};
 
 pub fn middle_day(year: u32) -> Option<Weekday> {
     let is_leap = (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
-    let middle_day = if is_leap { 184 } else { 183 };
-    let date = NaiveDate::from_yo_opt(year as i32, middle_day)?;
+    if is_leap {
+        return None;
+    }
+    let date = NaiveDate::from_yo_opt(year as i32, 183)?;
     Some(date.weekday())
 }
