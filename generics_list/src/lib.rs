11b0800ab86011db0ae01cb8bc12f7c88eefbcb2
Aymane Berhili <aberhili0man@gmail.com>
2025-09-20 10:04:18 +0100

    I thought we have done with linked lists

---
+++ generics_list/src/lib.rs
@@ -0,0 +1,41 @@
+#[derive(Clone, Debug)]
+pub struct List<T> {
+    pub head: Option<Box<Node<T>>>,
+}
+
+#[derive(Clone, Debug)]
+pub struct Node<T> {
+    pub value: T,
+    pub next: Option<Box<Node<T>>>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    pub fn push(&mut self, value: T) {
+        let new_node = Box::new(Node {
+            value,
+            next: self.head.take(),
+        });
+        self.head = Some(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|node| {
+            self.head = node.next;
+            node.value
+        })
+    }
+
+    pub fn len(&self) -> usize {
+        let mut count = 0;
+        let mut current = &self.head;
+        while let Some(node) = current {
+            count += 1;
+            current = &node.next;
+        }
+        count
+    }
+}
