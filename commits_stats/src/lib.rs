ea44d1a4f24d22c9b64500454d164dce2080847c
Aymane Berhili <aberhili0man@gmail.com>
2025-09-21 14:48:01 +0100

    I used username instead of full name :

--- commits_stats/src/lib.rs
+++ commits_stats/src/lib.rs
@@ -17,7 +17,7 @@ pub fn commits_per_author(data: &json::JsonValue) -> HashMap<String, u32> {
     let mut author_counts = HashMap::new();
     if let json::JsonValue::Array(commits) = data {
         for commit in commits {
-            if let Some(author_name) = commit["commit"]["author"]["name"].as_str() {
+            if let Some(author_name) = commit["author"]["login"].as_str() {
                 *author_counts.entry(author_name.to_string()).or_insert(0) += 1;
             }
         }
