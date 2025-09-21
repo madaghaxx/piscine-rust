a21e4a6c3dccc5dae50c91ce57bb352a64109b38
Aymane Berhili <aberhili0man@gmail.com>
2025-09-21 11:19:44 +0100

    pushing my old version because there is no way my 3 methods just failed like that

--- lalgebra_vector/src/lib.rs
+++ lalgebra_vector/src/lib.rs
@@ -1,38 +1,30 @@
-use std::ops::{ Add };
-use lalgebra_scalar::*;
+use std::ops::*;
+pub trait Scalar: Copy + Add<Output = Self> + Mul<Output = Self> {}
 
-#[derive(Debug, Clone, Eq, PartialEq)]
+impl<T: Copy + Add<Output = Self> + Mul<Output = Self>> Scalar for T {}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Vector<T: Scalar>(pub Vec<T>);
 
 impl<T: Scalar> Add for Vector<T> {
     type Output = Self;
+
     fn add(self, other: Self) -> Self::Output {
-        Vector(
+        if self.0.len() != other.0.len() {
+            return None;
+        }
+
+        let result: Vector<T> = Vector(
             self.0
-                .into_iter()
-                .zip(other.0)
-                .map(|(a, b)| a + b)
+                .iter()
+                .zip(other.0.iter())
+                .map(|(x, y)| x.clone() + y.clone())
                 .collect()
-        )
+        );
+        Some(result)
     }
 }
 
-// impl<T: Scalar + Default> Vector<T> {
-//     pub fn new() -> Self {
-//         Vector(Vec::new())
-//     }
-//     pub fn dot(&self, other: &Self) -> Option<T> {
-//         if self.0.len() != other.0.len() {
-//             return None;
-//         }
-
-//         let mut result = T::default();
-//         for (a, b) in self.0.iter().zip(other.0.iter()) {
-//             result = result + *a * *b;
-//         }
-//         Some(result)
-//     }
-// }
 impl<T: Scalar> Vector<T> {
     pub fn new() -> Self {
         Self(Vec::new())
