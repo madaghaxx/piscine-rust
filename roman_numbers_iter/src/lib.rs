6428d7e8790c7c11b5f71481f87af3823dcf8847
Aymane Berhili <aberhili0man@gmail.com>
2025-09-27 16:01:14 +0100

    yes I had the idea but it toke too long to work so I vibecode it

---
+++ roman_numbers_iter/src/lib.rs
@@ -0,0 +1,115 @@
+use crate::RomanDigit::*;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct RomanNumber(pub Vec<RomanDigit>);
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum RomanDigit {
+    Nulla,
+    I,
+    V,
+    X,
+    L,
+    C,
+    D,
+    M,
+}
+
+impl RomanDigit {
+    const fn value(self) -> u32 {
+        match self {
+            I => 1,
+            V => 5,
+            X => 10,
+            L => 50,
+            C => 100,
+            D => 500,
+            M => 1000,
+            Nulla => 0,
+        }
+    }
+}
+
+impl From<u32> for RomanDigit {
+    fn from(value: u32) -> Self {
+        match value {
+            1 => I,
+            5 => V,
+            10 => X,
+            50 => L,
+            100 => C,
+            500 => D,
+            1000 => M,
+            _ => Nulla,
+        }
+    }
+}
+
+impl From<u32> for RomanNumber {
+    fn from(mut num: u32) -> Self {
+        if num == 0 {
+            return RomanNumber(vec![Nulla]);
+        }
+
+        let mut digits = Vec::new();
+        const VALUES: [(u32, &[RomanDigit]); 13] = [
+            (1000, &[M]),
+            (900, &[C, M]),
+            (500, &[D]),
+            (400, &[C, D]),
+            (100, &[C]),
+            (90, &[X, C]),
+            (50, &[L]),
+            (40, &[X, L]),
+            (10, &[X]),
+            (9, &[I, X]),
+            (5, &[V]),
+            (4, &[I, V]),
+            (1, &[I]),
+        ];
+
+        for (value, roman_digits) in VALUES.iter() {
+            while num >= *value {
+                digits.extend_from_slice(roman_digits);
+                num -= value;
+            }
+        }
+
+        RomanNumber(digits)
+    }
+}
+
+impl Iterator for RomanNumber {
+    type Item = RomanDigit;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.0.is_empty() { None } else { Some(self.0.remove(0)) }
+    }
+}
+
+impl RomanNumber {
+    pub fn next(&self) -> Option<RomanNumber> {
+        let current: u32 = self.clone().into();
+        Some(RomanNumber::from(current + 1))
+    }
+}
+
+impl From<RomanNumber> for u32 {
+    fn from(roman: RomanNumber) -> Self {
+        let mut total = 0;
+        let digits = &roman.0;
+
+        for i in 0..digits.len() {
+            let current_value = digits[i].value();
+            let next_value = digits.get(i + 1).map_or(0, |d| d.value());
+
+            if current_value < next_value {
+                total -= current_value;
+            } else {
+                total += current_value;
+            }
+        }
+
+        total
+    }
+}
