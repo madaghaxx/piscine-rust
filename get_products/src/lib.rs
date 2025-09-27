552139abdf1a1b386beb382289c7efff63258bcf
Aymane Berhili <aberhili0man@gmail.com>
2025-09-27 11:09:26 +0100

    idk what are they testing

--- get_products/src/lib.rs
+++ get_products/src/lib.rs
@@ -7,7 +7,7 @@ pub fn get_products(arr: Vec<usize>) -> Vec<usize> {
         let mut temp = 1;
         for j in &arr {
             if *i != *j {
-                temp *= j;
+                temp *= *j;
             }
         }
         res.push(temp);
