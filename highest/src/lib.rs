fe27c4f00f6d1f0049f88dcc347b97c4609e3c6e
Aymane Berhili <aberhili0man@gmail.com>
2025-09-27 14:24:08 +0100

    yes

---
+++ highest/src/lib.rs
@@ -0,0 +1,30 @@
+#[derive(Debug,Clone,Copy)]
+pub struct Numbers<'a> {
+    numbers: &'a [u32],
+}
+
+impl<'a> Numbers<'a> {
+    pub fn new(numbers: &'a [u32]) -> Self {
+        Numbers {
+            numbers,
+        }
+    }
+
+    pub fn list(&self) -> &[u32] {
+        self.numbers
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.numbers.last().copied()
+    }
+
+    pub fn highest(&self) -> Option<u32> {
+        self.numbers.iter().max().copied()
+    }
+
+    pub fn highest_three(&self) -> Vec<u32> {
+        let mut sorted = self.numbers.to_vec();
+        sorted.sort();
+        sorted.into_iter().rev().take(3).collect()
+    }
+}
